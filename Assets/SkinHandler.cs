2380694a872558af46ebeb2370d8c5e882fa4949
FrankLin9981 <nevergiveup9981@gmail.com>
2020-11-03 02:52:37 +0800

    Add skin change handler. Now, we can switch between skins

---
+++ Assets/SkinHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkinHandler : MonoBehaviour
+{
+	public Animator animator;
+    public AnimatorOverrideController SwitchDefaultAnim;
+    public AnimatorOverrideController SwitchBAnim;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+		{
+			animator.runtimeAnimatorController = SwitchDefaultAnim as RuntimeAnimatorController;
+		}
+
+        if (Input.GetKeyDown(KeyCode.W))
+		{
+            animator.runtimeAnimatorController = SwitchBAnim as RuntimeAnimatorController;
+		}
+    }
+}
