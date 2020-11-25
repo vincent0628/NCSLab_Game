54038ae770354cfcbe53dc2cf220d09c690e926f
FrankLin9981 <nevergiveup9981@gmail.com>
2020-11-25 13:34:43 +0800

    Add level selection

--- Assets/Scripts/MenuCtrl.cs
+++ Assets/Scripts/MenuCtrl.cs
@@ -6,9 +6,26 @@ using UnityEngine.SceneManagement;
 public class MenuCtrl : MonoBehaviour
 {
     
+    public GameObject Panel;
+
+    public void OpenPanel()
+    {
+        if(Panel != null)
+        {
+            bool isActive = Panel.activeSelf;
+            Panel.SetActive(!isActive);
+        }
+    }
+
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
     }
     
 }
