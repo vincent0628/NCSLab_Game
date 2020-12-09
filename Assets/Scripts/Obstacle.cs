238db697509580d0eb650bbe666adab8fdb8e630
Bruce Wang <r08921075@ntu.edu.tw>
2020-12-09 15:56:14 +0800

    add meteor hammer prefab

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    public float thetaNaught = 90.0f;
+    public float ratio = 3.0f;
+    private float prev_theta;
+
+    void Start()
+    {
+    }
+    void FixedUpdate()
+    { 
+        float theta = thetaNaught * Mathf.Cos(Mathf.Sqrt(ratio) * Time.time);
+        transform.eulerAngles = new Vector3(0, 0,theta);
+    }
+}
